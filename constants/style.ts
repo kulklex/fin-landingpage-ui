f7fa57ce4c26a3470587c9460d157fe9d2ec97ef
kulklex <kulklex@gmail.com>
2023-03-15 18:25:04 +0100

    Footer Impl

--- constants/style.ts
+++ constants/style.ts
@@ -1,25 +1,25 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
-    heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
-    paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
+    heading2: "font-poppins font-semibold text-[40px]  md:text-xl lg:text-2xl text-white w-full",
+    paragraph: "font-poppins font-normal text-dimWhite text-[18px] md:text-[25px] leading-[30.8px]",
   
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start",
   
     paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-12 py-4",
+    paddingY: "py-16 md:py-6",
+    padding: "px-2 py-4",
   
     marginX: "sm:mx-16 mx-6",
-    marginY: "sm:my-16 my-6",
+    marginY: "my-2",
   };
   
   export const layout = {
     section: `flex md:flex-row flex-col ${styles.paddingY}`,
-    sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+    sectionReverse: `flex flex-1 md:flex-row flex-col-reverse ${styles.paddingY}`,
   
-    sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
+    sectionImgReverse: `flex-1 flex flex-col md:flex-row ${styles.flexCenter} md:mt-0 mt-10 relative`,
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
   
     sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
