531e5064ae8655aaf888bea01251bb4723885e81
kulklex <kulklex@gmail.com>
2023-03-15 09:47:16 +0100

    Components and Custom Styles setup

---
+++ components/CTA.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function CTA({}: Props) {
+  return (
+    <div>CTA</div>
+  )
+}
---
+++ components/index.ts
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Button from './Button'
+import FeedBackCard from './FeedBackCard';
+import GetStarted from './GetStarted';
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+  Button,
+  FeedBackCard,
+  GetStarted,
+};
